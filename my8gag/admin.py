c8c0752d82c34a4229dd551f777202f6a9355166
Tim Schulz <tim@getbej.com>
2022-04-05 13:19:37 +0000

    Set up url routing and added first view to 'home'

--- my8gag/admin.py
+++ my8gag/admin.py
@@ -3,6 +3,6 @@ from .models import Topic, Post, Comment
 
 # Register your models here.
 
-admin.site.register(Room)
 admin.site.register(Topic)
-admin.site.register(Message)
+admin.site.register(Post)
+admin.site.register(Comment)
