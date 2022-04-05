42c2b554d31d4ef1747b5e27abc0c2fdff35b127
Tim Schulz <tim@getbej.com>
2022-04-05 12:48:37 +0000

    Register Models on the admin page

--- my8gag/admin.py
+++ my8gag/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Topic, Post, Comment
 
 # Register your models here.
+
+admin.site.register(Room)
+admin.site.register(Topic)
+admin.site.register(Message)
