6c72f85f58df0eaab7071a2ab951aca8ef228d64
Tim Schulz <tim@getbej.com>
2022-04-06 12:30:42 +0000

    Add a post_form.html file to create a post, wire it up in views

--- my8gag/urls.py
+++ my8gag/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('create_post/', views.create_post, name="create_post")
 ]
--- my8gag/views.py
+++ my8gag/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'my8gag/feed.html')
+
+def create_post(request):
+    context = {}
+    return render(request, 'my8gag/post_form.html', context)
