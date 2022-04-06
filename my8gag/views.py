4fd8f6fea62a7932a33402710a341fd01e4b4487
Tim Schulz <tim@getbej.com>
2022-04-06 11:57:20 +0000

    Add a generic navbar to the base.html file and link it to a feed.html file in my app

--- my8gag/views.py
+++ my8gag/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'base.html')
+    return render(request, 'my8gag/feed.html')
